6bf74262fb7d524ef176f984e83c5789014e3ee5
Martin Walsh <martin.walsh@gmail.com>
2016-02-13 16:57:50 +0000

    P5 Scrolling World

--- HoppyBunny/GameScene.swift
+++ HoppyBunny/GameScene.swift
@@ -12,13 +12,19 @@ class GameScene: SKScene {
     
     var hero: SKSpriteNode!
     var sinceTouch: CFTimeInterval = 0
+    var scrollNode: SKNode!
+    
     let fixedDelta: CFTimeInterval = 1.0/60.0 /* 60 FPS */
+    let scrollSpeed: CGFloat = 100
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         
-        /* Recurisve node search for 'hero' */
+        /* Recursive node search for 'hero' as referenced */
         hero = self.childNodeWithName("//hero") as! SKSpriteNode
+        
+        /* Set reference to scroll node */
+        scrollNode = self.childNodeWithName("scrollNode")
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -30,32 +36,60 @@ class GameScene: SKScene {
         /* Apply subtle rotation */
         hero.physicsBody?.applyAngularImpulse(1)
 
+        /* Reset touch timer */
         sinceTouch = 0
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
         
+        /* Grab current velocity */
         let velocityY = hero.physicsBody?.velocity.dy ?? 0
         
-        /* Restrict vertical velocity */
-        if(velocityY > 400) {
+        /* Check and cap vertical velocity */
+        if velocityY > 400 {
             hero.physicsBody?.velocity.dy = 400
         }
         
         /* Apply falling rotation */
-        if (sinceTouch > 0.1) {
+        if sinceTouch > 0.1 {
             let impulse = -20000 * fixedDelta
             hero.physicsBody?.applyAngularImpulse(CGFloat(impulse))
         }
-        
+
         /* Clamp rotation */
         hero.zRotation.clamp(CGFloat(-90).degreesToRadians(),CGFloat(30).degreesToRadians())
         hero.physicsBody?.angularVelocity.clamp(-2, 2)
+    
+        /* Process world scrolling */
+        scrollWorld()
         
+        /* Update last touch timer */
         sinceTouch+=fixedDelta
     }
-
+    
+    func scrollWorld() {
+        /* Scroll World */
+        
+        scrollNode.position.x -= scrollSpeed * CGFloat(fixedDelta)
+        
+        /* Loop through scroll node sprites */
+        for node in scrollNode.children as! [SKSpriteNode] {
+            
+            /* Ground node position, convert node position to scene space */
+            let nodePosition = scrollNode.convertPoint(node.position, toNode: self)
+            
+            /* Check if ground sprite has left the scene */
+            if nodePosition.x <= -node.size.width / 2 {
+                
+                /* Reposition ground sprite to the second starting position */
+                let newPosition = CGPointMake( (self.size.width / 2) + node.size.width, nodePosition.y)
+                
+                /* Convert new node position back to scroll node space */
+                node.position = self.convertPoint(newPosition, toNode: scrollNode)
+            }
+        }
+    }
 }
 
 
